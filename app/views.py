0ebc73c6673df2a36b864ca11816ee3ef1cb69fa
derrickmacharia <derrick.macharia@student.moringaschool.com>
2021-10-31 02:50:27 +0300

    add macro python function

--- app/views.py
+++ app/views.py
@@ -11,10 +11,13 @@ def index():
     View root page function that returns the index page and its data
     '''
     # Getting popular movie
-    source = get_sources()
+    new_source = get_sources()
+    buisness_source = get_sources()
+    entertainment_source = get_sources()
+   # sports_source = get_sources()
     # print(source)
     title = 'Home - Welcome to The best Movie Review Website Online'
-    return render_template('index.html', title = title,sources = source)
+    return render_template('index.html', title = title,new_sources = new_source, buisness_sources = buisness_source , entertainment_sources = entertainment_source )
 
 @app.route('/sources/<int:sources_id>')
 def sources(sources_id):
@@ -23,3 +26,4 @@ def sources(sources_id):
     View news page function that returns the news details page and its data
     '''
     return render_template('sources.html',id = sources_id)
+# entertainment_sources = entertainment_source, sports_sources = sports_source
